edbeff3c2318b2335554ad70240468a78f4b3c5b
Mirek <mirekstudnik@gmail.com>
2026-03-17 16:55:10 +0100

    Configure production Meilisearch on Railway

--- search_config.js
+++ search_config.js
@@ -1,7 +1,7 @@
 // Konfigurace pro vyhledávání
 // Pro Netlify: Změňte tyto hodnoty na adresu vašeho hostovaného Meilisearch serveru
 const SEARCH_CONFIG = {
-    host: 'http://127.0.0.1:7700',
-    apiKey: '', // Veřejný vyhledávací klíč (Search Key)
+    host: 'https://getmeilimeilisearchv190-production-9296.up.railway.app',
+    apiKey: 'wdmelraygehzat2x6w4808h9znwz6qr8', // Veřejný vyhledávací klíč (Search Key)
     indexName: 'products'
 };
